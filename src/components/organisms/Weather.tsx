8dc5d15508519b3c3b699fcbd79bc75e8957bb33
hatsu38 <hajiwata0308@gmail.com>
2021-04-26 01:47:46 +0900

    Add Daily Weather

--- src/components/organisms/Weather.tsx
+++ src/components/organisms/Weather.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import DayJs from "../../libs/dayjs-ja";
 
-interface Weather {
-  description: string,
-  icon: string,
-}
 interface currentWeather {
   temperature: number
-  weather: Weather,
+  description: string,
+  icon: string,
 }
 
 interface dailyWeather {
   date: Date,
+  probabilityOfPrecipitation: number,
+  description: string,
+  icon: string,
   temperature: {
     max: number,
     min: number
   },
-  probabilityOfPrecipitation: number,
-  weather: Weather,
 }
 
 function Weather() {
@@ -44,11 +43,10 @@ function Weather() {
     const data = response.data;
     setCurrentWeather({
       temperature: Math.round(data.current.temp),
-      weather: {
-        description: data.current.weather[0].description,
-        icon: data.current.weather[0].icon,
-      }
+      description: data.current.weather[0].description,
+      icon: data.current.weather[0].icon,
     });
+
     const formattedDailyWeather = data.daily.map((weather: any) => (
       formatDailyWeather(weather)
     ))
@@ -56,38 +54,49 @@ function Weather() {
   }
 
   const formatDailyWeather = (weather: any) => {
+    const date = DayJs.unix(weather.dt);
     return ({
-      date: weather.dt,
+      date: `${date.format("DD")}(${date.format("dd")})`,
+      probabilityOfPrecipitation: weather.pop * 100,
+      description: weather.weather[0].description,
+      icon: weather.weather[0].icon,
       temperature: {
         max: Math.round(weather.temp.max),
         min: Math.round(weather.temp.min),
-      },
-      probabilityOfPrecipitation: weather.pop * 100,
-      weather: {
-        description: weather.weather[0].description,
-        icon: weather.weather[0].icon,
       },
     })
   }
 
   const weatherIconUrl = (icon: string) => {
-    console.log("icon", icon);
-    return `http://openweathermap.org/img/wn/${icon}@2x.png`
+    return `http://openweathermap.org/img/wn/${icon}.png`
   }
 
   return (
     <div className="font-light font-robot text-center">
       {currentWeather &&
         <>
-          <p className="text-thinGray">{currentWeather.weather.description}</p>
+          <p className="text-thinGray">{currentWeather.description}</p>
           <h2 className="text-8xl">{currentWeather.temperature}<span className="text-2xl text-thinGray">℃</span></h2>
-          <img src={weatherIconUrl(currentWeather.weather.icon)} className="mx-auto" />
+          <img src={weatherIconUrl(currentWeather.icon)} className="mx-auto" />
         </>
       }
       {dailyWeather.length > 0 &&
-        <div className="text-2xl flex justify-between space-x-2">
+        <div className="text-2xl flex justify-center space-x-2">
           <div>最高: {dailyWeather[0].temperature.max}<span className="text-sm text-thinGray">℃</span></div>
           <div>最低: {dailyWeather[0].temperature.min}<span className="text-sm text-thinGray">℃</span></div>
+        </div>
+      }
+      {dailyWeather.length > 0 &&
+        <div className="mt-4 space-y-2">
+        {dailyWeather.map((weather, index) => (
+          <div className="text-2xl flex justify-between items-center space-x-2 border-b-2 border-thinGray" key={index}>
+            <div>{weather.date}</div>
+            <img src={weatherIconUrl(weather.icon)} className="mx-auto" />
+            <div>{weather.temperature.max}<span className="text-sm text-thinGray">℃</span></div>
+            <div>{weather.temperature.min}<span className="text-sm text-thinGray">℃</span></div>
+            <div>{weather.probabilityOfPrecipitation}<span className="text-sm text-thinGray">%</span></div>
+          </div>
+        ))}
         </div>
       }
     </div>
