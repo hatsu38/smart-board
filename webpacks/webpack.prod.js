64c3b95c1ed3d34bcbbeaeca189fe140660f101e
hatsu38 <hajiwata0308@gmail.com>
2021-04-27 01:41:11 +0900

    Add Weather Hourly

--- webpacks/webpack.prod.js
+++ webpacks/webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-
-const dist = path.join(__dirname, "dist");
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.OPEN_WEATHER_API_KEY': JSON.stringify(process.env.OPEN_WEATHER_API_KEY),
+    }),
+  ]
 });
 
 
